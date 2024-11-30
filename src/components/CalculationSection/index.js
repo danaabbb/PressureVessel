56a0e19865c3d6f30951b51cc721a5bf6cd54c56
Massoud Nohra <massoud.nohra@net.usj.edu.lb>
2024-11-30 08:55:15 +0200

    Components refactoring

--- src/components/CalculationSection/index.js
+++ src/components/CalculationSection/index.js
@@ -1,40 +1,34 @@
-import React from 'react'
-import { Button } from '../ButtonElement'
-import { ImgWrap } from '../ModelingSection/Elements.js'
-import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, Img} from '../ModelingSection/Elements.js'
-import { motion } from "framer-motion";
-import { Link, useNavigate } from 'react-router-dom';
-const CalculationSection = ({ showIcon, lightBg, id, imgStart, topline, lightText, headline, darkText, description, img, alt, buttonLabel,dark, dark2 , primary }) => {
-  const navigate =useNavigate();
+import React from 'react';
+import { Button } from '../ButtonElement';
+import { ImgWrap, InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, Img } from '../ModelingSection/Elements.js';
+import { Link } from 'react-router-dom';
+
+const CalculationSection = ({ lightBg, id, imgStart, topline, lightText, headline, description, img, alt, buttonLabel }) => {
   return (
-    <>
-   
-    <InfoContainer lightBg={lightBg} id={id} > 
-    <InfoWrapper>
-        <InfoRow imgStart= {imgStart}>
-            <Column1>
+    <InfoContainer lightBg={lightBg} id={id}>
+      <InfoWrapper>
+        <InfoRow imgStart={imgStart}>
+          <Column1>
             <TextWrapper>
-                <TopLine>{ topline}</TopLine>
-                <Heading lightText= {lightText}> {headline}  </Heading>
-                <Subtitle lightBg = {lightBg} > {description} </Subtitle>
-                <BtnWrap> 
-                   <Link to='/calculations' style={{ textDecoration: 'none' }}> 
-                   <Button> {buttonLabel} </Button> 
-                   </Link>
-                    </BtnWrap>
+              <TopLine>{topline}</TopLine>
+              <Heading lightText={lightText}>{headline}</Heading>
+              <Subtitle lightBg={lightBg}>{description}</Subtitle>
+              <BtnWrap>
+                <Link to='/calculations' style={{ textDecoration: 'none' }}>
+                  <Button>{buttonLabel}</Button>
+                </Link>
+              </BtnWrap>
             </TextWrapper>
-            </Column1>
-            <Column2>
+          </Column1>
+          <Column2>
             <ImgWrap>
-            <Img src={img} alt={alt} />
+              <Img src={img} alt={alt} />
             </ImgWrap>
-            </Column2>
+          </Column2>
         </InfoRow>
-    </InfoWrapper>
+      </InfoWrapper>
     </InfoContainer>
-    </>
-  )
-}
+  );
+};
 
-
-export default CalculationSection
+export default CalculationSection;
